80062d3be36ad382b55b47254be1aca9f3808018
Callum Robertson <callumr1@hotmail.com>
2016-08-15 10:43:03 +1000

    Add Prac 1 files and name_check for Prac 2

---
+++ Prac 2/name_check.py
@@ -0,0 +1,20 @@
+def main():
+    import random
+
+    def get_name():
+        name = str(input("Please enter your name: "))
+        if name == "":
+            print("You didn't enter your name!")
+
+        else:
+            n = random.randint(1, len(name))
+            print_name(name,n)
+
+    def print_name(name,n):
+        print(name[::n])
+
+    name = get_name()
+
+
+
+main()
