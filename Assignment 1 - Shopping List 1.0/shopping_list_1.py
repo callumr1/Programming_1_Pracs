109d95e05d87e08271f7bd1f96705a1070c6e252
Callum Robertson <callumr1@hotmail.com>
2016-09-05 10:02:22 +1000

    Add Prac 6 materials and additions to assignment

--- Assignment 1 - Shopping List 1.0/shopping_list_1.py
+++ Assignment 1 - Shopping List 1.0/shopping_list_1.py
@@ -5,6 +5,7 @@ Written by Callum Robertson
 
 
 def main():
+    list_file = open("items.csv", "r")
     users_name = "Callum"
     print("Greetings {} and welcome to your shopping list!\n".format(users_name))
 
@@ -15,18 +16,18 @@ def main():
 
     while menu_condition != "5":
         if menu_condition == "1":
-            list_file = open("items.csv", "r")
-            print(list_file.readline())
+            required_items(list_file)
             print("Required items and total price")
             print(menu)
             menu_condition = str(input(">>>"))
+
         elif menu_condition == "2":
             print("Completed items and total cost")
             print(menu)
             menu_condition = str(input(">>>"))
+
         elif menu_condition == "3":
             print("Add an item")
-            list_file = open("items.csv", "a")
             new_item = str(input("Enter a new item: "))
             new_item_price = float(input("Enter the cost of the new item: "))
             item_importance = int(input("Enter the importance of the item on a scale of 1-3: "))
@@ -34,12 +35,22 @@ def main():
             list_file.close()
             print(menu)
             menu_condition = str(input(">>>"))
+
         elif menu_condition == "4":
             print("Change an item from required to completed")
+
         else:
             print("Please select from one of the 5 menu options: ")
             print(menu)
             menu_condition = str(input(">>>"))
+
+
+def required_items(list_file):
+    line_str = list_file.readline()
+    print(line_str)
+    for line_str in list_file:
+        if ",r" in line_str:
+            print(line_str)
 
 
 main()
