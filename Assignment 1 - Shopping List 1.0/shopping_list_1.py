e9b9a93d8a3344f0f511e364f14b6795440714fb
Callum Robertson <callumr1@hotmail.com>
2016-09-09 14:35:17 +1000

    Add final changes to shopping_list-1

--- Assignment 1 - Shopping List 1.0/shopping_list_1.py
+++ Assignment 1 - Shopping List 1.0/shopping_list_1.py
@@ -13,11 +13,11 @@ https://github.com/callumr1/Programming_1_Pracs/tree/master/Assignment%201%20-%2
 
 def main():
     users_name = "Callum"
-    print("Greetings {} and welcome to your shopping list!\n".format(users_name))
+    print("Greetings {} and Welcome to your Shopping List!\n".format(users_name))
 
     menu = "1 - View Required items\n2 - View Completed items\n3 - Add an item\n4 - Change an item from Required to Completed\n5 - Exit your shopping list"
 
-    print("What would you like to do? Enter either 1, 2, 3 or 4 from the menu options below:")
+    print("What would you like to do? Enter 1 through to 5 from the menu options below:")
     print(menu)
     menu_condition = str(input(">>>"))
     # Items list
@@ -46,7 +46,7 @@ def main():
 
         elif menu_condition == "4":
             # Asks the user for the name of the item they want to change and sends it to the change_item function
-            print("What item would you like to change to completed?")
+            print("\nWhat item would you like to change to completed?")
             item_name = str(input(">>> "))
             change_item(items, item_name)
             menu_condition = print_menu(menu, menu_condition)
@@ -96,20 +96,21 @@ def add_item(items):
     new_item.append("r")
     items.append(new_item)
 
-    print("Your new shopping list looks like this:")
-    for item in items:
-        print(item)
-
 
 def refine_items(items, item_needed):
     # Searches for either the required items or the completed ones depending on what item_needed is
-    line_str = items
-    if item_needed in line_str:
-        print(line_str)
-    for line_str in items:
-        if item_needed in line_str:
-            item = line_str
-            print(item[:-1])
+    total_cost = 0.0
+    longest_name = max(len(item) for item in items)
+    sorted_items = sorted(items, key=lambda item: item[2])
+    for item in sorted_items:
+        if item_needed in item:
+
+            print('{:20} $ {:6.2f} {:>3}{}{}'.format(str(item[0]), float(item[1]), "(", int(item[2]), ")"))
+            total_cost += float(item[1])
+    print("{:20} $ {:6.2f}".format("The total cost is:", total_cost))
+    # if item_needed in line_str:
+    # item = line_str
+    # print(item[:-1])
 
 
 """
@@ -132,11 +133,8 @@ def change_item(items, item_name):
             new_line_str = item[:-1]
             new_line_str.append("c")
             items.remove(item)
-    print("{} was change to completed \n".format(item_name))
-    items.append(new_line_str)
-    print("Your new shopping list looks like this:")
-    for item in items:
-        print(item)
+            print("{} was change to completed".format(item_name))
+            items.append(new_line_str)
 
 
 """
